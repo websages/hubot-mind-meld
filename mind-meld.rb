eee870e1bc6e5e7c50c5cec39f7fee34fe0a4580
Stephen Yeargin <stephen.yeargin@gmail.com>
2019-08-30 11:24:23 -0500

    Fixes

--- mind-meld.rb
+++ mind-meld.rb
@@ -2,6 +2,8 @@ require 'date'
 require 'json'
 require 'redis'
 
+timestamp = DateTime.now.strftime('%Y%jT%H%MZ')
+
 redis = Redis.new
 json = redis.get('hubot:storage')
 
@@ -15,18 +17,24 @@ brain = JSON.parse(json)
 # Placeholder for new brain objects
 users = {}
 
-# Clone brain, clear users field
-merged = brain.clone
+# Copy brain, clear users field
+merged = brain.dup
 merged['users'] = users
 
 brain['users'].each do |id, user|
-  if merged['users'][user['name']].nil?
-    merged['users'][user['name']] = user
-  else
+  if merged['users'].key?(user['name'])
     merged['users'][user['name']] = merged['users'][user['name']].merge(user)
+  else
+    merged['users'][user['name']] = user
   end
+
   # Set the ID field to match the screen name
   merged['users'][user['name']]['id'] = user['name']
 end
 
-redis.set('hubot:storage', merged.to_json)
+File.open("hubot-storage-merged-#{timestamp}.json", 'w') do |f|
+  f.write merged.to_json
+  f.close
+end
+
+puts redis.set('hubot:storage', merged.to_json)
